92561e3b76a546e11d3b8d2928e86a080294bddc
christian.tavera <christian.tavera@grupoargos.com.mx>
2020-12-08 15:46:41 -0500

    Initial commit.

---
+++ src/main/java/da/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -0,0 +1,18 @@
+package da.springframework.msscbrewery.services;
+
+import da.springframework.msscbrewery.web.model.BeerDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class BeerServiceImpl implements BeerService {
+
+    @Override
+    public BeerDTO getBeerById(UUID beerId) {
+        return BeerDTO.builder().id(UUID.randomUUID())
+                .beerName("Galaxy Cat")
+                .beerStyle("Pale Ale")
+                .build();
+    }
+}
