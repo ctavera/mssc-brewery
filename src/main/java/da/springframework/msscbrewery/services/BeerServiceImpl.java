6a5d08f1befe728a1d85c9d8c0bbfa72cbb79f70
christian.tavera <christian.tavera@grupoargos.com.mx>
2020-12-09 11:38:29 -0500

    34. HTTP DELETE with Spring MVC.

--- src/main/java/da/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/da/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package da.springframework.msscbrewery.services;
 
 import da.springframework.msscbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
 
@@ -26,5 +28,10 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
         //todo impl - would add a real impl to save beer
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer...");
     }
 }
--- src/main/java/da/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/da/springframework/msscbrewery/web/controller/BeerController.java
@@ -44,4 +44,11 @@ public class BeerController {
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
+
+    @DeleteMapping({"/{beerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void handleDelete (@PathVariable("beerId") UUID beerId) {
+
+        beerService.deleteById(beerId);
+    }
 }
