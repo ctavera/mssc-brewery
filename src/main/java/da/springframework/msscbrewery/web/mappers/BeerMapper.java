1adfdca763f89ad22fc11c017aaa82e709fceb8c
christian.tavera <christian.tavera@grupoargos.com.mx>
2020-12-15 15:58:27 -0500

    78. Example of using Project Lombok and MapStruct.

---
+++ src/main/java/da/springframework/msscbrewery/web/mappers/BeerMapper.java
@@ -0,0 +1,13 @@
+package da.springframework.msscbrewery.web.mappers;
+
+import da.springframework.msscbrewery.domain.Beer;
+import da.springframework.msscbrewery.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto dto);
+}
---
+++ target/generated-sources/annotations/da/springframework/msscbrewery/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,54 @@
+package da.springframework.msscbrewery.web.mappers;
+
+import da.springframework.msscbrewery.domain.Beer;
+import da.springframework.msscbrewery.domain.Beer.BeerBuilder;
+import da.springframework.msscbrewery.web.model.BeerDto;
+import da.springframework.msscbrewery.web.model.BeerDto.BeerDtoBuilder;
+import da.springframework.msscbrewery.web.model.v2.BeerStyleEnum;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-12-15T15:27:19-0500",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 11.0.9 (Oracle Corporation)"
+)
+@Component
+public class BeerMapperImpl implements BeerMapper {
+
+    @Override
+    public BeerDto beerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDtoBuilder beerDto = BeerDto.builder();
+
+        beerDto.id( beer.getId() );
+        beerDto.beerName( beer.getBeerName() );
+        if ( beer.getBeerStyle() != null ) {
+            beerDto.beerStyle( beer.getBeerStyle().name() );
+        }
+        beerDto.upc( beer.getUpc() );
+
+        return beerDto.build();
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        BeerBuilder beer = Beer.builder();
+
+        beer.id( dto.getId() );
+        beer.beerName( dto.getBeerName() );
+        if ( dto.getBeerStyle() != null ) {
+            beer.beerStyle( Enum.valueOf( BeerStyleEnum.class, dto.getBeerStyle() ) );
+        }
+        beer.upc( dto.getUpc() );
+
+        return beer.build();
+    }
+}
